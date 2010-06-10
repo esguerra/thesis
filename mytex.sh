b38beb8f2fa21e1f6a6d0ff9da31882235756bab
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2010-06-10 11:37:29 -0400

    Chapter 3. Getting it done.

--- mytex.sh
+++ mytex.sh
@@ -1,7 +1,7 @@
 B#!/bin/bash
 bibtex Chapter1/chapter1
 bibtex Chapter2/chapter2
-1bibtex Chapter3/chapter3
+bibtex Chapter3/chapter3
 bibtex Chapter4/chapter4
 #bibtex Chapter5/chapter5
 #bibtex Chapter6/chapter6
@@ -9,6 +9,7 @@ bibtex main
 bibtex Appendix/appendix1a
 bibtex Appendix/appendix2a
 bibtex Appendix/appendix2b
+bibtex Appendix/appendix2c
 bibtex Appendix/appendix4a
 makeindex main
 pdflatex main
