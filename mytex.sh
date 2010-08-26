dcba5821455c8eb00ef31b95ecb439437dd03187
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2010-08-26 11:38:39 -0400

    chapter five integrated into main context of thesis.

--- mytex.sh
+++ mytex.sh
@@ -3,8 +3,7 @@ bibtex Chapter1/chapter1
 bibtex Chapter2/chapter2
 bibtex Chapter3/chapter3
 bibtex Chapter4/chapter4
-#bibtex Chapter5/chapter5
-#bibtex Chapter6/chapter6
+bibtex Chapter5/chapter5
 bibtex main
 bibtex Appendix/appendix1a
 bibtex Appendix/appendix2a
