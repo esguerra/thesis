42b0fdb082defbb0a9e70face1ab914501086856
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2016-06-15 17:27:35 +0200

    Adding a github repository and cls fix The thesis compiles again after commenting out a redefinition in ruformat.cls of xfloat. Also, an error in the abstract is fixed and hyperref has been activated again.

--- mytex.sh
+++ mytex.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#set -x
+#trap read debug
+rm -f *.aux *.log *.idx *.lof *.lot *.toc main.pdf
+rm -f Chapter*/*.aux *.log *.idx *.lof *.lot *.toc main.pdf *.bbl *.blg 
+pdflatex main
 bibtex Chapter1/chapter1
 bibtex Chapter2/chapter2
 bibtex Chapter3/chapter3
@@ -9,8 +14,10 @@ bibtex Appendix/appendix1a
 bibtex Appendix/appendix2a
 bibtex Appendix/appendix2b
 bibtex Appendix/appendix4a
-#bibtex Appendix/appendix5a
+bibtex Appendix/appendix5a
+bibtex main
 makeindex main
 pdflatex main
 pdflatex main
 #xpdf main.pdf
+#/sw/bin/xpdf main.pdf
